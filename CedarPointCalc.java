30c670ac4cd59bbbcee9ce1d74c6dedcf4bbd2a3
cdixon513 <corinne.dixon@hilliardschools.org>
2018-09-11 09:46:34 -0400

    Money format added

--- CedarPointCalc.java
+++ CedarPointCalc.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class CedarPointCalc{
 	public static void main(String[] args){
 		//Declare variables
 		int numOfStudents, numOfChaperones;
+		DecimalFormat money = new DecimalFormat("$.00");
 		double fallCost, springCost, charterBusCost, schoolBusCost, foodCost;
 		final double schoolBusCostPerPerson = 1300.42 / 44; //school buses are $1300.42 per 44 person bus
 		final double charterBusCostPerPerson = 1738.92 /55; //charter buses are $1738.92 per 55 person bus
@@ -38,12 +40,12 @@ public class CedarPointCalc{
 		System.out.printf("%15s", "Spring Cost");
 		System.out.println();
 		System.out.printf("%15s", "School Bus");
-		System.out.printf("%15.2f", fallCost + schoolBusCost + foodCost);
-		System.out.printf("%15.2f", springCost + schoolBusCost + foodCost);
+		System.out.printf("%15s", money.format(fallCost + schoolBusCost + foodCost));
+		System.out.printf("%15s", money.format(springCost + schoolBusCost + foodCost));
 		System.out.println();
 		System.out.printf("%15s", "Charter Bus");
-		System.out.printf("%15.2f", fallCost + charterBusCost + foodCost);
-		System.out.printf("%15.2f", springCost + charterBusCost + foodCost);
+		System.out.printf("%15s", money.format(fallCost + charterBusCost + foodCost));
+		System.out.printf("%15s", money.format(springCost + charterBusCost + foodCost));
 		System.out.println();
 	}
 }
